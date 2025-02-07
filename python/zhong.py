88af50f9ba0e6a87bf83631b84db58793d163a76
阿明 <1191552161@qq.com>
2025-02-07 18:17:16 +0800

    w

--- python/zhong.py
+++ python/zhong.py
@@ -9,15 +9,15 @@
 import pygame
 
 #初始化调音器
-# pygame.mixer.init()
+pygame.mixer.init()
 
-# #加载音乐文件
-# pygame.mixer.music.load("1.mp3")
-# #播放音乐文件
-# pygame.mixer.music.play()
-# # 等待播放完成
-# while pygame.mixer.music.get_busy():
-#     pygame.time.Clock().tick(10)
+#加载音乐文件
+pygame.mixer.music.load("1.mp3")
+#播放音乐文件
+pygame.mixer.music.play()
+# 等待播放完成
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
 
 
 #3. 
